e9d39dbb0b84a00922ad4b73365e359002656009
Alan Clark <alan@thatscotdatasci.com>
2020-11-10 16:22:03 +0000

    Simplifying config

--- setup.sh
+++ setup.sh
@@ -2,12 +2,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"user@example.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 " > ~/.streamlit/config.toml
